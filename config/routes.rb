48f00c6cf720f37107d09451e9bb096e52b1cd55
wuhlcom <378433855@qq.com>
2016-07-31 10:03:27 -0400

    comments

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,9 @@ Rails.application.routes.draw do
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
-     resources :articles
+     resources :articles do
+	resources :comments
+     end
   # Example resource route with options:
   #   resources :products do
   #     member do
